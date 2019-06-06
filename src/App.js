317a05c6b1ef4bf9b0ce53caa9aa98bb969542b4
Yusuke Kurokawa <krkwy.sk.dev@gmail.com>
2019-06-06 23:58:26 +0800

    Cool App

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cool app coming up soon
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more
         </a>
       </header>
     </div>
